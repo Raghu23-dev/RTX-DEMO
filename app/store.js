9737ca1d4120c8369cfec3bd064e7ca56d8d35c1
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-13 11:29:58 +0530

    Initialized

---
+++ app/store.js
@@ -0,0 +1,12 @@
+
+const configureStore = require('@reduxjs/toolkit').configureStore
+const { coreModule } = require('@reduxjs/toolkit/query')
+const cakeReducer = require('./features/cake/cakeSlice')
+
+  store = configureStore({
+    reducer: {
+        cake:cakeReducer
+    }
+})
+
+module.exports = store
---
+++ index.js
@@ -0,0 +1,17 @@
+//npm install @reduxjs/toolkit
+
+const store = require('./app/store')
+const cakeActions = require('./app/features/cake/cakeSlice').cakeActions
+
+
+console.log(store.getState())
+const unSubscribe = store.subscribe(()=>{
+    console.log('updated state ',store.getState())
+})
+
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+unSubscribe()
