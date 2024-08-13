db4d446ac1167aa2728380b693679302a63d18a6
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-13 12:31:50 +0530

    Added redux-logger as a middle ware (cmd: npm install redux-thunk)

--- app/store.js
+++ app/store.js
@@ -3,13 +3,16 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const { coreModule } = require('@reduxjs/toolkit/query')
 const cakeReducer = require('./features/cake/cakeSlice')
 const iceCreamReducer = require('./features/icecream/icecreamSlice')
+const reduxlogger=require('redux-logger')
 
+const logger = reduxlogger.createLogger();
   store = configureStore({
     reducer: {
         cake:cakeReducer,
         icecream:iceCreamReducer
         
-    }
+    },
+    middleware: (getdefaultMiddleware)=>getdefaultMiddleware().concat(logger),
 })
 
 module.exports = store
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const store = require('./app/store')
 const cakeActions = require('./app/features/cake/cakeSlice').cakeActions
 
 
-console.log(store.getState())
+
+//console.log(store.getState())
 const unSubscribe = store.subscribe(()=>{
-    console.log('updated state ',store.getState())
+    //console.log('updated state ',store.getState())
 })
 
 
