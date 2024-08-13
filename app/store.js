8d88b34ec6c2faaae8d3b0a331cfd2593244f2e6
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-13 11:25:16 +0530

    Clearing

--- app/store.js
+++
@@ -1,9 +0,0 @@
-const configureStore = require('@reduxjs/toolkit').configureStore
-const cakeReducer = require('./features/cake/cakeSlice')
-
-const store = configureStore({
-    reducer: {
-        cake:cakeReducer
-    }
-})
-//export default store
